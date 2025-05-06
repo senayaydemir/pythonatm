7b4f999ffc79f9bd6df04895aab7dfa54684a93e
senayaydemir <senayaydemir00@gmail.com>
2025-05-06 23:54:27 +0300

    Kodda güncelleme yapıldı

--- banka.py
+++ banka.py
@@ -1,3 +1,8 @@
 kartsifresi="2002"
 bakiye="5000"
 print("X Bankasına Hoşgeldiniz")
+İslemDurumu = True
+while İslemDurumu:
+ girilenSifre=input("Kart Şifrenizi Giriniz:")
+if girilenSifre != kartsifresi:
+    print("Yanlış Şifre.Tekrar Deneyiniz.")
