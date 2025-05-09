a5145af7374d21c025b788249c08ea2f98b713a6
senayaydemir <senayaydemir00@gmail.com>
2025-05-10 00:55:00 +0300

    Güncelleme Yapıldı

--- banka.py
+++ banka.py
@@ -1,22 +1,22 @@
 kartsifresi="2002"
-bakiye="5000"
-denemeHakki="3"
+bakiye= 5000
+denemeHakki= 3
 print("X Bankasına Hoşgeldiniz")
 kart_islem_durumu=True
 İslemDurumu = True
 while İslemDurumu:
- girilenSifre=input("Kart Şifrenizi Giriniz:")
-if girilenSifre != kartsifresi:
-    print("Yanlış Şifre.Tekrar Deneyiniz.")
-    denemeHakki -=1
-    print(denemeHakki,"Deneme Hakkınız Kaldı")
-    if denemeHakki == 0:
-       print("Kartınız Bloklandı.Banka ile görüşün")
+    girilenSifre=input("Kart Şifrenizi Giriniz:")
+    if girilenSifre != kartsifresi:
+       print("Yanlış Şifre.Tekrar Deneyiniz.")
+       denemeHakki -=1
+       print(denemeHakki,"Deneme Hakkınız Kaldı")
+       if denemeHakki == 0:
+          print("Kartınız Bloklandı.Banka ile görüşün")
        İslemDurumu=False
-else:
-   while kart_islem_durumu:
-      print("Giriş Yapıldı")    
-      print("""
+    else:
+         while kart_islem_durumu:
+           print("Giriş Yapıldı")    
+           print("""
             Yapılacak İşlemi Seçiniz
             ------------------------
             1.Para Çekme
@@ -25,21 +25,21 @@ else:
             4.Çıkış
             ------------------------
             """)
-      islemNo =input("İşlem numarasını giriniz:")
-      if islemNo =="4":
-         print("Çıkış Yapıldı")
-         İslemDurumu = False
-         kart_islem_durumu = False
-      elif islemNo == "3":
-         print("Toplam Bakiye:",bakiye,"₺")
-      elif islemNo == "2":
-         yatırılacakmiktar=int(input("Yatırılacak Miktar:"))
-         bakiye += yatırılacakmiktar
-         print("İşlem Gerçekleşti")
-      elif islemNo =="1":
-         cekilecekmiktar = int(input("Çekilecek Miktar:"))
-         if cekilecekmiktar > bakiye:
-            print("Yetersiz Bakiye") 
-         else:
+           islemNo =input("İşlem numarasını giriniz:")
+           if islemNo =="4":
+             print("Çıkış Yapıldı")
+             İslemDurumu = False
+             kart_islem_durumu = False
+           elif islemNo == "3":
+             print("Toplam Bakiye:",bakiye,"₺")
+           elif islemNo == "2":
+            yatırılacakmiktar=int(input("Yatırılacak Miktar:"))
+            bakiye += yatırılacakmiktar
+            print("İşlem Gerçekleşti")
+           elif islemNo =="1":
+            cekilecekmiktar = int(input("Çekilecek Miktar:"))
+            if cekilecekmiktar > bakiye:
+             print("Yetersiz Bakiye") 
+           else:
             bakiye -=cekilecekmiktar
             print("İşlem Gerçekleşti")
